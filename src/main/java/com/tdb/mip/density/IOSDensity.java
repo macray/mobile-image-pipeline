454a5e3794641270c61d32d205af9a9c300e21d1
Vaudaux-Ruth Guillaume <guillaume.vaudauxruth@fastconnect.fr>
2016-04-28 11:57:44 +0200

    Fix bug in iOS densities

--- src/main/java/com/tdb/mip/density/IOSDensity.java
+++ src/main/java/com/tdb/mip/density/IOSDensity.java
@@ -5,9 +5,9 @@ import java.util.Collections;
 import java.util.List;
 
 public class IOSDensity {
-	public static Density X1 = new Density("1x", "", 0.75f);
-	public static Density X2 = new Density("2x", "@2x", 1f);
-	public static Density X3 = new Density("3x", "@3x", 1.5f);
+	public static Density X1 = new Density("1x", "", 1f);
+	public static Density X2 = new Density("2x", "@2x", 2f);
+	public static Density X3 = new Density("3x", "@3x", 3f);
 
 	public static List<Density> ALL = Collections.unmodifiableList( //
 			Arrays.asList(X1, X2, X3) //
